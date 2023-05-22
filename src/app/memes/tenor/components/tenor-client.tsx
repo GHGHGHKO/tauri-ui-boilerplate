fd3a4722b567702df683dec8d8b09b43e0cf61bd
HG_Ko <gudrb963@gmail.com>
2023-05-22 20:32:48 +0900

    feat: Perform `Get Memes` when Enter or Return is pressed (#10)

--- src/app/memes/tenor/components/tenor-client.tsx
+++ src/app/memes/tenor/components/tenor-client.tsx
@@ -40,6 +40,15 @@ export function Tenor() {
   const [limit, setLimit] = useState("")
   const [openDialog, setOpenDialog] = useState(false);
 
+  const onFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      e.preventDefault();
+      await tenor_call();
+    } catch (err: any) {
+      console.error(err);
+    }
+  }
+
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   async function tenor_call() {
     try {
@@ -53,68 +62,70 @@ export function Tenor() {
   }
 
   return (
-      <div className="flex-1 space-y-4">
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-          <Card className={cn("w-[500px]")}>
-            <CardContent className="grid gap-4 p-1.5">
-              <div className="grid gap-2">
-                <Label htmlFor="search">Let's toss some meme keywords, folks!</Label>
-                <Input id="search" type="text" placeholder="Fimally!"
-                       onChange={(e) => setQuery(e.target.value)} />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="password">
-                  Where might my key be hiding, Gustaf?{" "}
-                  <a
-                      href="https://github.com/GHGHGHKO/tauri-get-memes/wiki/I-hate-key-input"
-                      className="font-medium text-primary underline underline-offset-4"
-                      target="_blank" rel="noopener noreferrer">
-                    I hate key input
-                  </a>
-                </Label>
-                <Input id="password" type="password" placeholder="xxxxxxACSKexUfqoKqSQxxxxxxh63zdC25ubpuA"
-                       onChange={(e) => setKey(e.target.value)} />
-              </div>
-              <div className="grid gap-2">
-                <Label htmlFor="search">Hit me with your best shot!!</Label>
-                <Input id="limit" type="text" placeholder="1~50"
-                       onChange={(e) => setLimit(e.target.value)} />
-              </div>
-            </CardContent>
-            <Dialog open={openDialog} onOpenChange={setOpenDialog}>
-              <CardFooter>
-                <Button className="w-full" onClick={tenor_call}>Get Memes</Button>
-              </CardFooter>
-              <DialogContent>
-                <DialogHeader>
-                  <DialogTitle>Oops!</DialogTitle>
-                  <DialogDescription>
-                    {errors.error.message}
-                  </DialogDescription>
-                </DialogHeader>
-              </DialogContent>
-            </Dialog>
-          </Card>
-        </div>
-        <div>
-          <Separator className="my-4" />
-          <div className="relative">
-            <ScrollArea>
-              <div className="flex flex-wrap gap-x-4 pb-4">
-                {results.results.map((gif) => (
-                    <TenorGifs
-                        key={gif.media_formats.tinygif.url}
-                        gifFields={gif.media_formats.tinygif}
-                        className="w-[250px] my-4"
-                        aspectRatio="portrait"
-                        width={250}
-                        height={330}
-                    />
-                ))}
-              </div>
-            </ScrollArea>
+      <form onSubmit={onFormSubmit}>
+        <div className="flex-1 space-y-4">
+          <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+            <Card className={cn("w-[500px]")}>
+              <CardContent className="grid gap-4 p-1.5">
+                <div className="grid gap-2">
+                  <Label htmlFor="search">Let's toss some meme keywords, folks!</Label>
+                  <Input id="search" type="text" placeholder="Fimally!"
+                         onChange={(e) => setQuery(e.target.value)} />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="password">
+                    Where might my key be hiding, Gustaf?{" "}
+                    <a
+                        href="https://github.com/GHGHGHKO/tauri-get-memes/wiki/I-hate-key-input"
+                        className="font-medium text-primary underline underline-offset-4"
+                        target="_blank" rel="noopener noreferrer">
+                      I hate key input
+                    </a>
+                  </Label>
+                  <Input id="password" type="password" placeholder="xxxxxxACSKexUfqoKqSQxxxxxxh63zdC25ubpuA"
+                         onChange={(e) => setKey(e.target.value)} />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="search">Hit me with your best shot!!</Label>
+                  <Input id="limit" type="text" placeholder="1~50"
+                         onChange={(e) => setLimit(e.target.value)} />
+                </div>
+              </CardContent>
+              <Dialog open={openDialog} onOpenChange={setOpenDialog}>
+                <CardFooter>
+                  <Button className="w-full">Get Memes</Button>
+                </CardFooter>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Oops!</DialogTitle>
+                    <DialogDescription>
+                      {errors.error.message}
+                    </DialogDescription>
+                  </DialogHeader>
+                </DialogContent>
+              </Dialog>
+            </Card>
+          </div>
+          <div>
+            <Separator className="my-4" />
+            <div className="relative">
+              <ScrollArea>
+                <div className="flex flex-wrap gap-x-4 pb-4">
+                  {results.results.map((gif) => (
+                      <TenorGifs
+                          key={gif.media_formats.tinygif.url}
+                          gifFields={gif.media_formats.tinygif}
+                          className="w-[250px] my-4"
+                          aspectRatio="portrait"
+                          width={250}
+                          height={330}
+                      />
+                  ))}
+                </div>
+              </ScrollArea>
+            </div>
           </div>
         </div>
-      </div>
+      </form>
   )
 }
