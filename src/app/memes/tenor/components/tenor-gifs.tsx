3086ade802ed2fb09bcb63b301fe7526a15ad65f
GHGHGHKO <gudrb963@gmail.com>
2023-05-27 17:34:12 +0900

    feat: right-click on GIF to enable copy and download functionality

--- src/app/memes/tenor/components/tenor-gifs.tsx
+++ src/app/memes/tenor/components/tenor-gifs.tsx
@@ -1,19 +1,7 @@
 import Image from "next/image"
-import { ListMusic, PlusCircle } from "lucide-react"
 
 import { cn } from "@/lib/utils"
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuSeparator,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
-  ContextMenuTrigger,
-} from "@/components/ui/context-menu"
 
-import { gifsList } from "../data/gifs-list"
 import { GifFields } from "@/app/memes/tenor/data/tenor-fields";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Button } from "@/components/ui/button";
@@ -46,47 +34,18 @@ export function TenorGifs({
   }
   return (
     <div className={cn("space-y-3", className)} {...props}>
-      <ContextMenu>
-        <ContextMenuTrigger>
-          <div className="overflow-hidden rounded-md">
-            <Image
-              src={gifFields.url}
-              alt={gifFields.url}
-              width={width}
-              height={height}
-              className={cn(
-                "h-auto w-auto object-cover transition-all hover:scale-105",
-                aspectRatio === "portrait" ? "aspect-[3/4]" : "aspect-square"
-              )}
-            />
-          </div>
-        </ContextMenuTrigger>
-        <ContextMenuContent className="w-40">
-          <ContextMenuItem>Add to Library</ContextMenuItem>
-          <ContextMenuSub>
-            <ContextMenuSubTrigger>Add to Playlist</ContextMenuSubTrigger>
-            <ContextMenuSubContent className="w-48">
-              <ContextMenuItem>
-                <PlusCircle className="mr-2 h-4 w-4" />
-                New Playlist
-              </ContextMenuItem>
-              <ContextMenuSeparator />
-              {gifsList.map((gifs) => (
-                <ContextMenuItem key={gifs}>
-                  <ListMusic className="mr-2 h-4 w-4" /> {gifs}
-                </ContextMenuItem>
-              ))}
-            </ContextMenuSubContent>
-          </ContextMenuSub>
-          <ContextMenuSeparator />
-          <ContextMenuItem>Play Next</ContextMenuItem>
-          <ContextMenuItem>Play Later</ContextMenuItem>
-          <ContextMenuItem>Create Station</ContextMenuItem>
-          <ContextMenuSeparator />
-          <ContextMenuItem>Like</ContextMenuItem>
-          <ContextMenuItem>Share</ContextMenuItem>
-        </ContextMenuContent>
-      </ContextMenu>
+      <div className="overflow-hidden rounded-md">
+        <Image
+          src={gifFields.url}
+          alt={gifFields.url}
+          width={width}
+          height={height}
+          className={cn(
+            "h-auto w-auto object-cover transition-all hover:scale-105",
+            aspectRatio === "portrait" ? "aspect-[3/4]" : "aspect-square"
+          )}
+        />
+      </div>
       <div className="flex space-x-2">
         <Button onClick={() => handleClick(gifFields.url)} variant="secondary" className="shrink-0">
           Download Gif
