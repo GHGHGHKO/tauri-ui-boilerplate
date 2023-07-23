fbc0bdd57aa2678704dc9884713df756c56ba751
GHGHGHKO <gudrb963@gmail.com>
2023-07-23 13:21:51 +0900

    refactor: Change `env` to `&str`

--- src/app/memes/tenor/components/tenor-client.tsx
+++ src/app/memes/tenor/components/tenor-client.tsx
@@ -36,7 +36,6 @@ export function Tenor() {
   const [results, setResults] = useState<TenorResults>({ results: [] })
   const [errors, setError] = useState<TenorError>(initialState)
   const [query, setQuery] = useState("welcome")
-  const [key, setKey] = useState("")
   const [limit, setLimit] = useState("50")
   const [openDialog, setOpenDialog] = useState(false);
 
@@ -52,7 +51,7 @@ export function Tenor() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
   async function tenor_call() {
     try {
-      setResults(await invoke("tenor_call", {query, key, limit}));
+      setResults(await invoke("tenor_call", {query, limit}));
       console.log(results);
     } catch (error) {
       setError(error)
