b5409de021a7df80288c4a87888647bf5e7fcfc3
GHGHGHKO <gudrb963@gmail.com>
2023-07-31 20:54:41 +0900

    chore: Remove unnecessary logs

--- src-tauri/src/dotenv_file/make_dotenv.rs
+++ src-tauri/src/dotenv_file/make_dotenv.rs
@@ -7,11 +7,9 @@ pub fn check_or_create_dotenv() -> Result<bool, bool> {
 
     match Path::new(file_name).exists() {
         true => {
-            println!("exist");
             Ok(false)
         }
         false => {
-            println!("not exist");
             match File::create(file_name) {
                 Ok(_) => Ok(false),
                 Err(_) => Err(true),
