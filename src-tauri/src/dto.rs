9d2cad8ec1b376f07e32a689ddd4be8e73970058
GHGHGHKO <gudrb963@gmail.com>
2023-09-08 21:19:17 +0900

    Revert "chore: mod files"
    This reverts commit c1d2105fa2558fd4236a40836be3a33e9ae161a6.

--- src-tauri/src/dto.rs
+++ src-tauri/src/dto.rs
@@ -4,5 +4,3 @@ pub mod github_error_response;
 pub mod github_latest_release_response;
 pub mod giphy_response;
 pub mod giphy_error_response;
-pub mod gfycat_error_response;
-pub mod gfycat_response;
